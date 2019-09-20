384f7c8cf4a61c8199787f5354b814f0a4529b1f
Васин Юрий <george.a.wise@gmail.com>
2019-09-20 17:43:09 +0300

    Update postgre.sql

--- postgre.sql
+++ postgre.sql
@@ -1,13 +1,9 @@
 /*
-From ORACLE
-
+ORACLE with DUAL dummy table
 https://rextester.com/WXXK49768
-https://rextester.com/JEATP10708
-
 https://rextester.com/LPDK29606
 https://rextester.com/HWL98099
 https://rextester.com/NON43124
-
 based on discussion
 https://www.sql.ru/forum/1313400/raschyot-zadolzhennosti-po-lifo
 
@@ -27,33 +23,47 @@ CREATE TABLE Loans (
     Sum INT
 );
 
-/* 2017-08-15 */
-
 INSERT INTO Loans VALUES
-	('2009-12-12', 111110, 111111, 'RUR', 12000),
-	('2009-12-25', 111110, 111111, 'RUR', 5000),
-	('2009-12-12', 111110, 122222, 'RUR', 10000),
-	('2010-01-12', 111110, 111111, 'RUR', -10100),
-	('2009-11-20', 220000, 222221, 'RUR', 25000),
-	('2009-12-20', 220000, 222221, 'RUR', 20000),
-	('2009-12-31', 220001, 222221, 'RUR', -10000),
-	('2009-12-29', 111110, 122222, 'RUR', -10000),
-	('2009-11-27', 220001, 222221, 'RUR', -30000);
-  
-/* SELECT * FROM Loans; */
+    ('2009-12-12', 111110, 111111, 'RUR', 12000),
+    ('2009-12-25', 111110, 111111, 'RUR', 5000),
+    ('2010-01-12', 111110, 111111, 'RUR', -10100),
+    ('2009-12-12', 111110, 122222, 'RUR', 10000),
+    ('2009-12-14', 111110, 122222, 'RUR', -12000),
+    ('2009-12-16', 111110, 122222, 'RUR', -20000),
+    ('2009-12-29', 111110, 122222, 'RUR', -10000),
+    ('2009-11-20', 220000, 222221, 'RUR', 25000),
+    ('2009-11-27', 220001, 222221, 'RUR', -30000),
+    ('2009-12-20', 220000, 222221, 'RUR', 20000),
+    ('2009-12-31', 220001, 222221, 'RUR', -10000);
 
-/* SELECT 'https://rextester.com/WXXK49768' AS "ORACLE_realisation"; */
+/* WITH pdcl AS (SELECT * FROM Loans) */
 
-WITH pdcl AS (SELECT * FROM Loans)
-select Deal
-     , max(over_sum) as liability_sum
-     ---, DENSE_RANK() OVER ( partition by Deal order by Date desc) as liability_sum2
-     , min(case when exclude_sign is null then Date end) as liability_date
-  from (select Date
-             , Deal
-             , over_sum
-             , lead(nullif(sign(over_sum),1), 1, nullif(sign(over_sum),1)) over (partition by Deal order by Date) as exclude_sign
-          from ( select Date, Deal, sum(Sum) over(partition by Deal order by Date) as over_sum from pdcl
-               ) as th1
-       ) as th2
-group by Deal
+/*
+SELECT DISTINCT
+    Deal,
+    sum(Sum) OVER (PARTITION BY Deal) AS debt_per_deal
+FROM Loans
+ORDER BY Deal;
+*/
+
+SELECT cast(Date AS DATE), Sum FROM Loans WHERE Deal = 222221 ORDER BY Date;
+
+WITH b AS (SELECT
+    Deal,
+    Date,
+    sum(Sum) OVER (PARTITION BY Deal ORDER By Date ASC) AS balance,
+    Row_Number() OVER (PARTITION BY Deal ORDER By Date ASC) AS RowNum
+FROM Loans
+ORDER BY Deal DESC, Date
+           ),
+s AS (SELECT *, sign(balance) AS sign FROM b
+--- WHERE balance < 0 OR RowNum = 1; ---- qualify by number (first)
+      )
+SELECT *,
+CASE WHEN lag(sign) 
+       OVER (ORDER BY Deal DESC) != sign 
+       THEN RowNum END AS lo,
+  CASE WHEN lead(sign) 
+       OVER (ORDER BY Deal DESC) != sign 
+       THEN coalesce(RowNum, 0) END AS hi
+FROM s
